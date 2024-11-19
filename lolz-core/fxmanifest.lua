6b904656131848f1fa885006ab76439b6ceb01a9
RickyBGamez <rickyb5@live.ca>
2024-11-18 17:50:50 -0700

    create fxmanifest.lua

---
+++ lolz-core/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+name "lolz-core"
+description "a core/framework thing that i'm making. refer to the github for a better summary."
+author "ricky"
+version "0.0.0"
+
+shared_scripts {
+    "**/sh_*.lua"
+}
+
+client_scripts {
+    "**/cl_*.lua"
+}
+
+server_scripts {
+    "**/sv_*.lua"
+}
