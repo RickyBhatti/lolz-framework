8cb46709c51cdfdf7cfde0096523d4fe01e4f0d8
RickyBGamez <rickyb5@live.ca>
2025-09-08 18:46:03 -0600

    update fxmanifest.lua

--- lolz-core/fxmanifest.lua
+++ lolz-core/fxmanifest.lua
@@ -1,11 +1,13 @@
 fx_version "cerulean"
 game "gta5"
 lua54 "yes"
+use_experimental_fxv2_oal "yes"
 
 name "lolz-core"
 description "a core/framework thing that i'm making. refer to the github for a better summary."
 author "ricky"
 version "0.0.0"
+repository "https://github.com/RickyBhatti/lolz-framework/tree/main/lolz-core"
 
 shared_scripts {
     "locale/*.lua",
@@ -27,6 +29,4 @@ server_scripts {
     "server/sv_main.lua"
 }
 
-dependencies {
-    "oxmysql"
-}
+dependencies {} -- None for now. Actually, maybe no resources for the "core" or whatever this is. Basically, make this the base library/framework that other resources can depend on. Hmmmmm, that might mean I have to adjust some of the modules. Bluh, later me issue (we love tech debt!) lol.
